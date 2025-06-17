d794854c282bedca7b66416e9d690ef0c32f2cde
patleserdev <leserpat@hotmail.com>
2025-06-17 12:03:40 +0200

    add promise type and use in proxy methods

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -33,36 +33,41 @@ async function handler(req: NextRequest, path: string[]) {
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }
 ) {
-  return handler(req, params.path);
+  const { path } = await params;
+  return handler(req, path);
 }
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }
 ) {
-  return handler(req, params.path);
+  const { path } = await params;
+  return handler(req, path);
 }
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }
 ) {
-  return handler(req, params.path);
+  const { path } = await params;
+  return handler(req, path);
 }
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }
 ) {
-  return handler(req, params.path);
+  const { path } = await params;
+  return handler(req, path);
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }
 ) {
-  return handler(req, params.path);
+  const { path } = await params;
+  return handler(req, path);
 }
 
