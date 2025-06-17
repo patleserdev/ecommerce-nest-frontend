ad90310e89d42631965fe3941a1a63366bd0c71f
patleserdev <leserpat@hotmail.com>
2025-06-17 14:46:39 +0200

    rerechange proxy methods

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -1,26 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_API_BACKEND;
 
 async function handler(req: NextRequest, path: string[]) {
   if (!BACKEND_BASE_URL) {
-    return new NextResponse('Missing BACKEND_BASE_URL env variable', { status: 500 });
+    return new NextResponse("Missing BACKEND_BASE_URL env variable", {
+      status: 500,
+    });
   }
   const method = req.method;
   const search = req.nextUrl.search;
-  const targetUrl = `${BACKEND_BASE_URL}/${path.join('/')}${search}`;
+  const targetUrl = `${BACKEND_BASE_URL}/${path.join("/")}${search}`;
 
   const headers = new Headers(req.headers);
-  headers.set('cookie', req.headers.get('cookie') || '');
+  headers.set("cookie", req.headers.get("cookie") || "");
 
   const body =
-    method !== 'GET' && method !== 'HEAD' ? await req.text() : undefined;
+    method !== "GET" && method !== "HEAD" ? await req.text() : undefined;
 
   const backendRes = await fetch(targetUrl, {
     method,
     headers,
     body,
-    redirect: 'manual',
+    redirect: "manual",
   });
 
   const responseBody = await backendRes.text();
@@ -31,39 +33,32 @@ async function handler(req: NextRequest, path: string[]) {
   });
 }
 
+type Params = { params: Promise<{ path: string[] }> };
 
-type Params = { params: { path: string[] } };
-
-export async function GET(
-  req: NextRequest,
-  { params }: Params) {
-  return handler(req, params.path);
+export async function GET(req: NextRequest, { params }: Params) {
+  const path= (await params).path
+  return handler(req, path);
 }
 
-export async function POST(
-  req: NextRequest,
-  { params }: Params) {
-  return handler(req, params.path);
+export async function POST(req: NextRequest, { params }: Params) {
+  const path= (await params).path
+  return handler(req, path);
 }
 
-export async function PUT(
-  req: NextRequest,
-  { params }: Params) {
-  return handler(req, params.path);
+export async function PUT(req: NextRequest, { params }: Params) {
+  const path= (await params).path
+  return handler(req, path);
 }
 
-export async function PATCH(
-  req: NextRequest,
-  { params }: Params) {
-  return handler(req, params.path);
+export async function PATCH(req: NextRequest, { params }: Params) {
+  const path= (await params).path
+  return handler(req, path);
 }
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: Params) {
-  return handler(req, params.path);
+export async function DELETE(req: NextRequest, { params }: Params) {
+  const path= (await params).path
+  return handler(req, path);
 }
-
 
 // export async function GET(
 //   req: NextRequest,
@@ -104,6 +99,3 @@ export async function DELETE(
 //   const { path } = await params;
 //   return handler(req, path);
 // }
-
-
-
